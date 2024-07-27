d0417717f35f9bd4c2c3f5810f75740b7dc3083e
Ayussh Gupta <72377031+KingAyussh1@users.noreply.github.com>
2024-07-27 19:44:23 +0530

    Update sendNotification.js

--- flight-status-system/notifications/fcm/sendNotification.js
+++ flight-status-system/notifications/fcm/sendNotification.js
@@ -1,1 +1,23 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const message = {
+    notification: {
+        title: 'Flight Update',
+        body: 'Flight AA101 is delayed'
+    },
+    token: 'DEVICE_REGISTRATION_TOKEN'
+};
+
+admin.messaging().send(message)
+    .then((response) => {
+        console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+        console.error('Error sending message:', error);
+    });
 
