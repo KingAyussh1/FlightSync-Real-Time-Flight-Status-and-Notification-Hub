27c67048d2d719d563674c2f96d60970e6b709ce
Ayussh Gupta <72377031+KingAyussh1@users.noreply.github.com>
2024-07-27 19:43:31 +0530

    Update producer.js

--- flight-status-system/notifications/rabbitmq/producer.js
+++ flight-status-system/notifications/rabbitmq/producer.js
@@ -1,1 +1,28 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        const queue = 'flight_status';
+        const msg = 'Flight AA101 is delayed';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log('Sent:', msg);
+    });
+
+    setTimeout(function () {
+        connection.close();
+        process.exit(0);
+    }, 500);
+});
 
